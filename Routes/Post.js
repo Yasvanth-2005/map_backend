dfa32ec4a37727bb1b85d296dff97f5ad5ae9d4d
Yasvanth-2005 <yasvanthhanumantu1@gmail.com>
2024-08-13 21:57:08 +0530

    posts

--- Routes/Post.js
+++ Routes/Post.js
@@ -1,5 +1,18 @@
 import express from "express";
+import {
+  createPost,
+  editPost,
+  getRegionPosts,
+  getUserPosts,
+  postComment,
+} from "../Controllers/Post.js";
 
 const router = express.Router();
+
+router.post("/new", createPost);
+router.edit("/edit/:id", editPost);
+router.post("/comment/:id", postComment);
+router.get("/region/:id", getRegionPosts);
+router.get("/user/:id", getUserPosts);
 
 export default router;
